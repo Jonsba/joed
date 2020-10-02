efca8d73681ab32abea7e5c5b2d1afec824350cd
Jonas Baggett <jonas17b@gmail.com>
2020-10-02 14:26:57 +0200

    Initialisation with Qt Creator

---
+++ src/frontend/editor.h
@@ -0,0 +1,21 @@
+#ifndef EDITOR_H
+#define EDITOR_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Editor; }
+QT_END_NAMESPACE
+
+class Editor : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	Editor(QWidget *parent = nullptr);
+	~Editor();
+
+private:
+	Ui::Editor *ui;
+};
+#endif // EDITOR_H
